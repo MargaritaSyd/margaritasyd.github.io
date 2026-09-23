1d2fe987b1de113ae2259cb009db8714f08211cc
MargaritaSyd <margarita.syddall@gmail.com>
2026-09-23 13:47:32 -0300

    feat(build): update docs folder for new version in pages

---
+++ docs/_next/static/chunks/app/layout-156ee2ecf64580fc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},6236:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1290,23)),Promise.resolve().then(n.t.bind(n,9353,23))},9353:()=>{}},e=>{e.O(0,[130,741,441,255,358],()=>e(e.s=6236)),_N_E=e.O()}]);
